c6c0676000e28653f035683f7001029fc0b4278f
sudheer369 <53401028+sudheer369@users.noreply.github.com>
2019-07-28 07:37:37 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        matInv <- NULL            ##initialing matInv to NULL
+        set <- function(y) {      ##function to set matrix to x
+                x <<- y
+                matInv <<- NULL
+        }
+        get <- function() x       ## function to obtain matrix
+        setinverse <- function(inverse) matInv <<- inverse ## function to assign invMat value
+        getinverse <- function() matInv   ## function to get matInv
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer to the functions with the $ operator
 
 }
 
@@ -12,4 +21,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matInv <- x$getmean()
+        if(!is.null(matInv) { ## checks whether matrrix inverse value in cache 
+                message("getting cached data")
+                return(matInv)
+        }
+        data <- x$get()
+        matInv <- solve(data,  ## computes matrix inverse
+        x$setmean(matInv) 
+        mayInv
+        ## Return a matrix that is the inverse of 'x'
+
+
 }
